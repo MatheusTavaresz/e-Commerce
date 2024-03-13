b992b28f261997ce92d78eb8ab144b66cacd0ee9
matheustavaresz <mathtg2@gmail.com>
2024-03-13 15:30:53 -0300

    feat: Install Axios and testing first requests

--- imoveis-front/src/views/Home.vue
+++ imoveis-front/src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
-  <pre>{{ catalog }}</pre>
+  <div class="flex p-8 justify-center">
+    <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Procurar por propriedades" />
+
+    <div class="flex gap-1">
+      <router-link :to="{ name: 'byCities', params: {city}}" v-for="city in cities" :key="city">
+       <button class="border border-gray-300 min-w-24"> {{ city }}</button>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import {computed} from 'vue';
+import { computed, onMounted } from 'vue';
 import store from '../store';
+import axiosClient from '/axiosClient.js'
 
-const catalog = computed(() => store.state.properties )
+const catalog = computed(() => store.state.properties);
+const cities = ["Governador Celso Ramos", "Florianopolis"];
+
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list')
+  console.log(response.data)
+})
+
 </script>
---
+++ imoveis-front/src/views/PropertyList.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>House lists by keywoord</h1>
+</template>
